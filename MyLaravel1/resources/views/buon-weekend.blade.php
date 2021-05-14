ecfd0ddcc919a389dd1255f4c33dffdb19c4b327
Lorenzo <lorenzo.antonelli89@gmail.com>
2021-05-14 15:25:01 +0200

    esercizio finito

---
+++ MyLaravel1/resources/views/buon-weekend.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Buon Week-End</title>
+    <link rel="stylesheet" href="{{ asset('/css/app.css') }}">
+</head>
+
+<body>
+
+<div id="container">
+    <h1>
+        Buon Week-End
+    </h1>
+    <h2>
+        Il vostro JUMBOTRON &hearts;
+    </h2>
+</div>
+</body>
+
+</html>
